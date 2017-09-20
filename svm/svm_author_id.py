e113c74f517964827080ee5f475c1d9bd67e8961
Ivo Pereira <ipereira@e-goi.com>
2017-09-20 17:58:27 +0100

    Solving SVM exercise

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,40 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+#clf = svm.SVC(kernel="linear")
+clf = svm.SVC(kernel="rbf",C=10000.0)
+
+### reduces the length of the training dataset
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
+
+
+### train
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+### test
+t0 = time()
+pred = clf.predict(features_test)
+print("testing time:", round(time()-t0, 3), "s")
+
+
+### evaluate
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred,labels_test)
+print("accuracy:", round(acc,3))
+
+#quiz 36
+print("element 10: ", pred[10])
+print("element 26: ", pred[26])
+print("element 50: ", pred[50])
+
+#quiz 37
+lst = list(pred)
+print("total of Chris (1) class:  ", lst.count(1))
+
 #########################################################
 
 
